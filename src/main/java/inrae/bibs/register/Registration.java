eaedd1e597b09c60a1bfaf463a4b06c6d661f2f8
dlegland <david.legland@inrae.fr>
2026-08-27 15:30:20 +0200

    use interpolation classes for registration

--- src/main/java/inrae/bibs/register/Registration.java
+++ src/main/java/inrae/bibs/register/Registration.java
@@ -16,6 +16,10 @@ import ij.ImageStack;
 import ij.process.ImageProcessor;
 import inrae.bibs.register.image.Image3D;
 import inrae.bibs.register.image.Images3D;
+import inrae.bibs.register.interp.Function2D;
+import inrae.bibs.register.interp.Function3D;
+import inrae.bibs.register.interp.TransformedImage2D;
+import inrae.bibs.register.interp.TransformedImage3D;
 import inrae.bibs.register.io.JsonRegistrationWriter;
 
 /**
@@ -32,41 +36,31 @@ public class Registration
      * 
      * @param reference
      *            the image used for sampling the reference grid
-     * @param transform
+     * @param transfo
      *            the geometric transformation applied to each point in the
      *            reference image
      * @param movingImage
      *            the image to interpolate
      * @return a new image with the same size as the reference image
      */
-    public static final ImageProcessor computeTransformedImage(ImageProcessor reference, Transform2D transform, ImageProcessor movingImage)
+    public static final ImageProcessor computeTransformedImage(ImageProcessor reference, Transform2D transfo, ImageProcessor movingImage)
     {
         // retrieve result image size
         int sizeX = reference.getWidth();
         int sizeY = reference.getHeight();
         
-        // size of moving image
-        int sizeX2 = movingImage.getWidth();
-        int sizeY2 = movingImage.getHeight();
-        
         // create result image
         ImageProcessor result = reference.createProcessor(sizeX, sizeY);
+        
+        // Create interpolation class, that encapsulates both the image and the transform
+        Function2D interp = new TransformedImage2D(movingImage, transfo);
         
         // iterate over pixels of result image
         for (int y = 0; y < sizeY; y++)
         {
             for (int x = 0; x < sizeX; x++)
             {
-                Point2D p = transform.transform(new Point2D(x, y));
-                
-                // nearest-neighbor interpolation
-                int xi = (int) Math.round(p.x);
-                int yi = (int) Math.round(p.y);
-                
-                if (xi < 0 || xi >= sizeX2) continue;
-                if (yi < 0 || yi >= sizeY2) continue;
-                
-                result.setf(x, y, movingImage.getf(xi, yi));
+                result.setf(x, y, (float) interp.evaluate(x, y));
             }
         }
         return result;
@@ -78,14 +72,14 @@ public class Registration
      * 
      * @param reference
      *            the image used for sampling the reference grid
-     * @param transform
+     * @param transfo
      *            the geometric transformation applied to each point in the
      *            reference image
      * @param movingImage
      *            the image to interpolate
      * @return a new image with the same size as the reference image
      */
-    public static final Image3D computeTransformedImage(Image3D reference, Transform3D transform, Image3D movingImage)
+    public static final Image3D computeTransformedImage(Image3D reference, Transform3D transfo, Image3D movingImage)
     {
         // retrieve result image size
         int sizeX = reference.getSize(0);
@@ -113,7 +107,7 @@ public class Registration
                 for (int x = 0; x < sizeX; x++)
                 {
 //                    IJ.log("x = " + x);
-                    Point3D p = transform.transform(new Point3D(x, y, z));
+                    Point3D p = transfo.transform(new Point3D(x, y, z));
 
                     // nearest-neighbor interpolation
                     int xi = (int) Math.round(p.x);
@@ -141,14 +135,14 @@ public class Registration
      * 
      * @param refStack
      *            the image used for sampling the reference grid
-     * @param transform
+     * @param transfo
      *            the geometric transformation applied to each point in the
      *            reference image
      * @param movingStack
      *            the image to interpolate
      * @return a new image with the same size as the reference image
      */
-    public static final ImageStack computeTransformedImage(ImageStack refStack, Transform3D transform, ImageStack movingStack)
+    public static final ImageStack computeTransformedImage(ImageStack refStack, Transform3D transfo, ImageStack movingStack)
     {
         // retrieve result image size
         Image3D reference = Images3D.createWrapper(refStack);
@@ -156,15 +150,12 @@ public class Registration
         int sizeY = reference.getSize(1);
         int sizeZ = reference.getSize(2);
         
-        // size of moving image
-        Image3D movingImage = Images3D.createWrapper(movingStack);
-        int sizeX2 = movingImage.getSize(0);
-        int sizeY2 = movingImage.getSize(1);
-        int sizeZ2 = movingImage.getSize(2);
-        
         // create result image
         ImageStack resultStack = ImageStack.create(sizeX, sizeY, sizeZ, reference.getBitDepth());
         Image3D result = Images3D.createWrapper(resultStack);
+        
+        // Create interpolation class, that encapsulates both the image and the transform
+        Function3D interp = new TransformedImage3D(movingStack, transfo);
         
         // iterate over voxels of result image
         IJ.showStatus("Apply transform to image");
@@ -175,18 +166,7 @@ public class Registration
             {
                 for (int x = 0; x < sizeX; x++)
                 {
-                    Point3D p = transform.transform(new Point3D(x, y, z));
-
-                    // nearest-neighbor interpolation
-                    int xi = (int) Math.round(p.x);
-                    int yi = (int) Math.round(p.y);
-                    int zi = (int) Math.round(p.z);
-
-                    if (xi < 0 || xi >= sizeX2) continue;
-                    if (yi < 0 || yi >= sizeY2) continue;
-                    if (zi < 0 || zi >= sizeZ2) continue;
-
-                    result.setValue(x, y, z, movingImage.getValue(xi, yi, zi));
+                    result.setValue(x, y, z, interp.evaluate(x, y, z));
                 }
             }
         }
